f895a5cd1f7715c56381d9530575ae65fe334a9a
Finn-Rasmus Darge <50116756+FinnDarge@users.noreply.github.com>
2019-10-07 19:54:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+  document.getElementById("rocket").src = "images/liftoff.png"; // Ändert das Bild
+  document.getElementById("rocket").id = "shake"; // Fügt id shake zum Bild hinzu
+}
